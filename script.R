11ce80e108c5b70805e4468612a1f51e8d00a66c
ries9112 <32306488+ries9112@users.noreply.github.com>
2020-07-25 07:27:00 -0600

    Update script.R

--- script.R
+++ script.R
@@ -40,3 +40,27 @@ file_delete('crypto_plot.gif')
 
 # Save gif
 image_write(anim, path='crypto_plot.gif')
+
+# Second gif for last 2 days of data
+# Filter to last 2 days
+messari <- subset(messari, DateTimeColoradoMST > Sys.time()-60*60*24*2)
+
+# Make gganimated plot of ETH data:
+anim <- animate(ggplot(data = messari,
+               aes(x = as.POSIXct(DateTimeColoradoMST), y = PriceUSD, group = Name)) + 
+                geom_line() +
+                labs(subtitle=paste('Latest data collected on:', max(messari$DateTimeColoradoMST), ' - MST'),
+                     caption='Data source: messari.io') + 
+                stat_smooth() + 
+                theme_economist() +
+                xlab('Date Time Collected (Colorado - MST)') +
+                ylab('Price USD ($)') +
+                transition_states(Name) +
+                ggtitle(paste('{closest_state} Price ($) Past 2 Days (Through ',max(messari$DateTimeColoradoMST), ' - MST')) +
+                view_follow(),fps=1)
+
+# Delete animation before making new one
+#file_delete('crypto_plot_2.gif')
+
+# Save gif
+image_write(anim, path='crypto_plot_2.gif')
