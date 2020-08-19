2a3f1dc2ab4fb0e9d382f10133e178347b8ddb13
ries9112 <32306488+ries9112@users.noreply.github.com>
2020-08-19 17:14:49 -0600

    Update script.R

--- script.R
+++ script.R
@@ -1,4 +1,5 @@
-library(PredictCrypto)
+library(DBI)
+library(RMariaDB)
 library(ggplot2)
 library(anytime)
 library(ggthemes)
@@ -6,32 +7,53 @@ library(fs)
 library(gganimate)
 library(magick)
 
-# Pull Messari data:
-messari <- get_crypto_data()
+############# SQL CONNECTION ###############
+Sys.setenv(user=db_user, pswd=db_pswd,ipAddress=db_ip)
+getSqlConnection <- function(){
+  con <-
+    dbConnect(
+      RMariaDB::MariaDB(),
+      username = Sys.getenv('user'),
+      password = Sys.getenv('pswd'),
+      host = Sys.getenv('ipAddress'),
+      dbname = 'Octoparse'
+    )
+  return(con)
+}
+database_connection <- getSqlConnection()
+tables_list <- dbListTables(database_connection)
+query <- 'SELECT * FROM HitBTC_orderbook_USD WHERE (symbol = 'BTC' OR symbol = 'ETH' OR symbol = 'BTC' OR symbol = 'XRP' OR symbol = 'LINK' OR symbol = 'BCH' OR symbol = 'LTC' OR symbol = 'BSV' OR symbol = 'ADA' OR symbol = 'BNB' OR symbol = 'EOS' OR symbol = 'CRO' OR symbol = 'XTZ' OR symbol = 'XLM' OR symbol = 'TRX' OR symbol = 'XMR' OR symbol = 'LEO' OR symbol = 'ATOM' OR symbol = 'NEO' OR symbol = 'MIOTA') AND quote_currency = 'USD' ORDER BY Date DESC LIMIT 100000'
+hitBTC <- dbFetch(dbSendQuery(database_connection, query))
 
 # Filter data to top 20 ranked cryptos
-messari <- subset(messari, Rank < 20)
+#hitBTC <- subset(hitBTC, Rank < 20)
+
+# Unique data
+hitBTC <- distinct(hitBTC, pkey)
 
 # Remove USD coins:
-#messari <- subset(messari, Name != c(messari$Name[!grepl("USD", messari$Name)]))
+#hitBTC <- subset(hitBTC, Name != c(hitBTC$Name[!grepl("USD", hitBTC$Name)]))
 
 # Convert date/time
-messari$DateTimeColoradoMST <- as.POSIXct(messari$DateTimeColoradoMST, format="%Y-%m-%d %H:%M:%S")
+hitBTC$date_time_utc <- as.POSIXct(hitBTC$date_time_utc, format="%Y-%m-%d %H:%M:%S")
 
 # Filter data to last 31 days
-messari <- subset(messari, DateTimeColoradoMST > Sys.time()-60*60*24*31)
+hitBTC <- subset(hitBTC, date_time_utc > Sys.time()-60*60*24*31)
+
+# Resymbol price field
+hitBTC <- resymbol(hitBTC, PriceUSD = 'bid_1_price'
 
 # Make gganimated plot:
-anim <- animate(ggplot(data = messari,
-               aes(x = as.POSIXct(DateTimeColoradoMST), y = PriceUSD, group = Name)) + 
+anim <- animate(ggplot(data = hitBTC,
+               aes(x = as.POSIXct(date_time_utc), y = PriceUSD, group = symbol)) + 
                 geom_line() +
-                labs(subtitle=paste('Latest data collected on:', max(messari$DateTimeColoradoMST), ' - MST'),
-                     caption='Data source: messari.io') + 
+                labs(subtitle=paste('Latest data collected on:', max(hitBTC$date_time_utc), ' - MST'),
+                     caption='Data source: hitBTC.io') + 
                 stat_smooth() + 
                 theme_economist() +
                 xlab('Date Time Collected (Colorado - MST)') +
                 ylab('Price USD ($)') +
-                transition_states(Name) +
+                transition_states(symbol) +
                 ggtitle('{closest_state} Price ($) Past 31 Days') +
                 view_follow(),fps=1)
 
@@ -43,19 +65,19 @@ image_write(anim, path='crypto_plot.gif')
 
 # Second gif for last 2 days of data
 # Filter to last 2 days
-messari_2 <- subset(messari, DateTimeColoradoMST > Sys.time()-60*60*24*2)
+hitBTC_2 <- subset(hitBTC, date_time_utc > Sys.time()-60*60*24*2)
 
 # Make gganimated plot:
-anim <- animate(ggplot(data = messari_2,
-               aes(x = as.POSIXct(DateTimeColoradoMST), y = PriceUSD, group = Name)) + 
+anim <- animate(ggplot(data = hitBTC_2,
+               aes(x = as.POSIXct(date_time_utc), y = PriceUSD, group = symbol)) + 
                geom_line() +
                geom_point() +
-               labs(subtitle=paste('Latest data collected on:', max(messari_2$DateTimeColoradoMST), ' - MST'),
-                    caption='Data source: messari.io') + 
+               labs(subtitle=paste('Latest data collected on:', max(hitBTC_2$date_time_utc), ' - MST'),
+                    caption='Data source: hitBTC.io') + 
                theme_economist() +
                xlab('Date Time Collected (Colorado - MST)') +
                ylab('Price USD ($)') +
-               transition_states(Name) +
+               transition_states(symbol) +
                ggtitle('{closest_state} Price ($) Past 2 Days') +
                view_follow(),fps=1)
 
@@ -67,19 +89,19 @@ image_write(anim, path='crypto_plot_2.gif')
 
 # Make more charts
 # Filter to last 7 days
-#messari_7 <- subset(messari, DateTimeColoradoMST > Sys.time()-60*60*24*7)
+#hitBTC_7 <- subset(hitBTC, date_time_utc > Sys.time()-60*60*24*7)
 
 # Make gganimated plot of reported 24h volume:
-#anim <- animate(ggplot(data = messari_7,
-#               aes(x = as.POSIXct(DateTimeColoradoMST), y = Reported24hVolume, group = Name)) + 
+#anim <- animate(ggplot(data = hitBTC_7,
+#               aes(x = as.POSIXct(date_time_utc), y = Reported24hVolume, group = symbol)) + 
 #                geom_point() +
-#                labs(subtitle=paste('Latest data collected on:', max(messari_7$DateTimeColoradoMST), ' - MST'),
-#                     caption='Data source: messari.io') + 
+#                labs(subtitle=paste('Latest data collected on:', max(hitBTC_7$date_time_utc), ' - MST'),
+#                     caption='Data source: hitBTC.io') + 
 #                stat_smooth() + 
 #                theme_economist() +
 #                xlab('Date Time Collected (Colorado - MST)') +
 #                ylab('Reported Volume (24 hour period)') +
-#                transition_states(Name) +
+#                transition_states(symbol) +
 #                ggtitle('{closest_state} Reported Volume ($) Past 7 Days') +
 #                view_follow(),fps=1)
 
@@ -91,16 +113,16 @@ image_write(anim, path='crypto_plot_2.gif')
 
 
 # Make gganimated plot of GitHub Stars:
-#anim <- animate(ggplot(data = messari_7,
-#               aes(x = as.POSIXct(DateTimeColoradoMST), y = Git_Stars, group = Name)) + 
+#anim <- animate(ggplot(data = hitBTC_7,
+#               aes(x = as.POSIXct(date_time_utc), y = Git_Stars, group = symbol)) + 
 #                geom_point() +
-#                labs(subtitle=paste('Latest data collected on:', max(messari_7$DateTimeColoradoMST), ' - MST'),
-#                     caption='Data source: messari.io') + 
+#                labs(subtitle=paste('Latest data collected on:', max(hitBTC_7$date_time_utc), ' - MST'),
+#                     caption='Data source: hitBTC.io') + 
 #                stat_smooth() + 
 #                theme_economist() +
 #                xlab('Date Time Collected (Colorado - MST)') +
 #                ylab('GitHub Stars') +
-#                transition_states(Name) +
+#                transition_states(symbol) +
 #                ggtitle('{closest_state} - GitHub Stars - Past 7 Days') +
 #                view_follow(),fps=1)
 
@@ -111,16 +133,16 @@ image_write(anim, path='crypto_plot_2.gif')
 #image_write(anim, path='crypto_git_stars.gif')
 
 # Make gganimated plot of active addresses:
-#anim <- animate(ggplot(data = messari_7,
-#               aes(x = as.POSIXct(DateTimeColoradoMST), y = ActiveAddresses, group = Name)) + 
+#anim <- animate(ggplot(data = hitBTC_7,
+#               aes(x = as.POSIXct(date_time_utc), y = ActiveAddresses, group = symbol)) + 
 #                geom_point() +
-#                labs(subtitle=paste('Latest data collected on:', max(messari_7$DateTimeColoradoMST), ' - MST'),
-#                     caption='Data source: messari.io') + 
+#                labs(subtitle=paste('Latest data collected on:', max(hitBTC_7$date_time_utc), ' - MST'),
+#                     caption='Data source: hitBTC.io') + 
 #                stat_smooth() + 
 #                theme_economist() +
 #                xlab('Date Time Collected (Colorado - MST)') +
 #                ylab('Active Addresses') +
-#                transition_states(Name) +
+#                transition_states(symbol) +
 #                ggtitle('{closest_state} - Number of Active Addresses - Past 7 Days') +
 #                view_follow(),fps=1)
 
